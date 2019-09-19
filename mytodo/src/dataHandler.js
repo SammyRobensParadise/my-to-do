d1615378cf291af6c59916dc33d6817874ea135d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-09-18 20:56:50 -0700

    added intializer to the datahandler.js file

--- mytodo/src/dataHandler.js
+++ mytodo/src/dataHandler.js
@@ -1,10 +1,20 @@
+import * as firebase from 'firebase'
 export default class dataHander {
     constructor(props,state,sendData,recieveData){
-        this.state = {
+        var config = {
+            apiKey: "AIzaSyBQjROjDE6Kap7JF-BomwGBITtMunusvZM",
+            authDomain: "https://my-to-do-db.firebaseapp.com/",
+            databaseURL: "https://my-to-do-db.firebaseio.com/",
+            storageBucket: "http://my-to-do-db.appspot.com/"
+          };
+          firebase.initializeApp(config);
+                this.state = {
             localProps: props,
             referenceState: state,
             dataTobeSent: sendData,
-            dataHandlerTobeRecieved: recieveData
+            dataHandlerTobeRecieved: recieveData,
+            db: firebase.database()
         }
     }
+
 }
